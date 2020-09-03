ac7f9b98e87c1c6709a055201383cb99f76f943d
compiuta <christianr@rocketmail.com>
2020-09-03 15:27:41 -0700

    add fetch functions to pass to view

--- src/js/controllers/nutrients-controller.js
+++ src/js/controllers/nutrients-controller.js
@@ -1,5 +1,27 @@
 (function(window) {
     'use strict';
 
+    function fetchSearchData(foodItem) {
+        app.nutrientsModel.getSearchData(foodItem);
+
+        console.log('fetching data');
+    }
+
+    function fetchFoodData(foodId) {
+        app.nutrientsModel.getFoodData(foodId);
+    }
+
     console.log('controller init');
+
+    const nutrientsController = {
+        fetchSearchData: function(foodItem) {
+            fetchSearchData(foodItem);
+        },
+        fetchFoodData: function(foodId) {
+            fetchFoodData(foodId);
+        }
+    }
+
+    window.app = window.app || {};
+    window.app.nutrientsController = nutrientsController;
 })(window);
