2967bbfb52bdcd58cbe4b4dffef5c5de76b0403e
compiuta <christianr@rocketmail.com>
2020-09-03 15:28:02 -0700

    add get data functions

--- src/js/models/nutrients-model.js
+++ src/js/models/nutrients-model.js
@@ -1,5 +1,35 @@
 (function(window) {
     'use strict';
 
+    function getData(fetchUrl) {
+        fetch(fetchUrl)
+        .then(response => response.json())
+        .then(data => console.log(data));
+    }
+
+    function getSearchData(foodItem) {
+        const fetchUrl = `https://nutrients1.herokuapp.com/${foodItem}`;
+
+        getData(fetchUrl);
+    }
+
+    function getFoodData(foodId) {
+        const fetchUrl = `https://nutrients1.herokuapp.com/nutrients/${foodId}`;
+
+        getData(fetchUrl);
+    }
+
+    const nutrientsModel = {
+        getSearchData: function(foodItem) {
+            getSearchData(foodItem)
+        },
+        getFoodData: function(foodId) {
+            getSearchData(foodId);
+        }
+    };
+
     console.log('model init');
+
+    window.app = window.app || {};
+    window.app.nutrientsModel = nutrientsModel;
 })(window);
