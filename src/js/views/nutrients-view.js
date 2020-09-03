b313c870fadde500f56ca9776cd66f1ca249304a
compiuta <christianr@rocketmail.com>
2020-09-03 15:28:29 -0700

    add dom catch and event listeners for search

--- src/js/views/nutrients-view.js
+++ src/js/views/nutrients-view.js
@@ -1,5 +1,23 @@
 (function(window) {
     'use strict';
 
+    const searchFoodForm = document.querySelector('[data-searchFoodForm]');
+    const searchFoodInput = document.querySelector('[data-searchFoodInput]');
+
+    function searchFood(e) {
+        e.preventDefault();
+
+        const  searchInputValue = searchFoodInput.value;
+
+        searchFoodInput.value = '';
+
+        console.log(searchInputValue);
+
+        app.nutrientsController.fetchSearchData(searchInputValue);
+
+    }
+
+    searchFoodForm.addEventListener('submit', searchFood);
+
     console.log('view init');
 })(window);
